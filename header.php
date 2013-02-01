16bcad4ee7effb209dffb87d99836e135a487032
renzestacio <renzestacio11@gmail.com>
2013-02-01 06:34:25 -0800

    Create header.php
    dapat nasa folder na "include" yan bago magamit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+		<title>The Athenaeum</title>
+		<meta charset = "utf-8" />
+		<link rel = "stylesheet" type = "text/css" href = "css/main.css" />
+		<script src="js/jquery-1.8.2.js"></script>
+		<script src="js/jquery-ui-1.9.0.custom.min.js"></script>
+	</head>
+	
+	<body>
+		<header> 
+			<a href = "#"><h1>The Athenaeum</h1></a>
+		</header>
+		<br/>
+		
