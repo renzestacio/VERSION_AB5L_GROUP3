94f443da978876d7ba737b00562fb1103ab4a8d4
renzestacio <renzestacio11@gmail.com>
2013-02-01 06:34:57 -0800

    Create footer.php
    nasa "include" na folder din to

---
+++ footer.php
@@ -0,0 +1,12 @@
+  	
+		<br/>
+		<footer>
+			&copy; The Athenaeum	<br/>
+			| <a class = "footer_links" href = "index.php">Home</a> | 
+			<a class = "footer_links" href = "login.php">Log In</a> |  
+			<a class = "footer_links" href = "register.php">Sign Up</a> | 
+			<a class = "footer_links" href = "#">Search</a> | 
+		</footer>
+	</body>
+</html>
+
